8d30319931731edbbca38dcd18428cfe960784cb
0xfort77 <145624342+0xfort77@users.noreply.github.com>
2026-03-16 19:02:51 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -311,7 +311,7 @@ local KILLToggle = MainTab:CreateToggle({
             local worldrootradius = workspace:GetPartBoundsInRadius(HRP.Position, 180, includeparams, excludeparams)
 
             for _, v in ipairs(worldrootradius) do
-                if v:IsA("Part") and v.Name == "HumanoidRootPart" then
+                if v:IsA("Part") then --and v.Name == "HumanoidRootPart" then
                     --v.CanQuery = true
                     --print(v.Parent)
                     for _, item in pairs(game:GetService("Players").LocalPlayer.Inventory:GetChildren()) do
