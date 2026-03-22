2c1945044adeeb7356946f6cdc32cbe0b8c1b904
0xfort77 <145624342+0xfort77@users.noreply.github.com>
2026-03-21 22:40:14 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@ if game.PlaceId == place_id or game.PlaceId == party_placeid then
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local _Version = "Cyan-99 v1.t"
+local _Version = "Cyan-99 v1.t2"
 
 local Window = Rayfield:CreateWindow({
    Name = _Version,
@@ -771,14 +771,14 @@ local collectflowersButton = MainTab:CreateButton({
                 for _, f in pairs(flRing1:GetChildren()) do
 
                     if f.Name == "Flower" then
-                        univ_flower_clickbox = flower:FindFirstChild("Clickbox")
+                        univ_flower_clickbox = f:FindFirstChild("Clickbox")
 
                         firesignal(univ_flower_clickbox.MouseButton1Click)
                         task.wait()
                         local args = {
-	                        flower
+	                        f
                         }
-                        flower.Parent = nil
+                        --flower.Parent = nil
                         game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("RequestPickFlower"):InvokeServer(unpack(args))
                         task.wait() 
 
@@ -800,7 +800,7 @@ local collectflowersButton = MainTab:CreateButton({
                 local args = {
 	                flower
                 }
-                flower.Parent = nil
+                --flower.Parent = nil
                 game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("RequestPickFlower"):InvokeServer(unpack(args))
                 task.wait()
 
