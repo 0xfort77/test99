388b48206afa99d9eae75685d25b9ea42ae28a92
0xfort77 <145624342+0xfort77@users.noreply.github.com>
2026-03-23 02:02:57 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@ if game.PlaceId == place_id or game.PlaceId == party_placeid then
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local _Version = "Cyan-99 v1.t"
+local _Version = "Cyan-99 v1.tpArray"
 
 local Window = Rayfield:CreateWindow({
    Name = _Version,
@@ -155,13 +155,13 @@ local quest_att = player:GetAttributes()
 for ident, _ in pairs(quest_att) do
     print(tostring(ident).." - "..tostring(_))
 
-    if ident == "TrackedQuest" then
+    if ident == {"TrackedQuest"} then
         local quest1 = _
     end
-    if ident == "TrackedQuest2" then
+    if ident == {"TrackedQuest2"} then
         local quest2 = _
     end
-    if ident == "TrackedQuest3" then
+    if ident == {"TrackedQuest3"} then
         local quest3 = _
     end
 
