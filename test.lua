44e0756223ebfce531d9c5570cca97389d6173fb
0xfort77 <145624342+0xfort77@users.noreply.github.com>
2026-03-22 21:33:45 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@ if game.PlaceId == place_id or game.PlaceId == party_placeid then
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local _Version = "Cyan-99 v1.tl11"
+local _Version = "Cyan-99 v1.t12"
 
 local Window = Rayfield:CreateWindow({
    Name = _Version,
@@ -3100,20 +3100,20 @@ local univ_cframe = CFrame.new(0, 0.55, 0)
 local logwallsButton = BaseTab:CreateButton({
     Name = "Auto Place Log Walls",
     Callback = function()
-        
-        for _, blue in pairs(game:GetService("Players").LocalPlayer.Inventory:GetChildren()) do
+        for i = 1, 50 do
+            for _, blue in pairs(game:GetService("Players").LocalPlayer.Inventory:GetChildren()) do
             
-            if blue.Name == "Log Wall Blueprint" then
+                if blue.Name == "Log Wall Blueprint" then
                 
-                print("found log wall blueprint")
+                    print("found log wall blueprint")
 
-                for i = 1, 45 do
+                
                     print("loop attempt")
                     print(tostring(i))
 
                     local radius = 100
 
-                    local angle = i * (math.pi * 2) / 45
+                    local angle = i * (math.pi * 2) / 100
                     local x = univ_vector.X + radius * math.cos(angle)
                     local z = univ_vector.Z + radius * math.sin(angle)
 
@@ -3124,18 +3124,20 @@ local logwallsButton = BaseTab:CreateButton({
 	                    blue,
                     {
                         Valid = true,
-                        CFrame = CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(10), 0),
-                        Position = vector.create(x, univ_vector.Y, z) * Vector3.new(0, math.rad(10), 0)
+                        CFrame = CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(math.rad(10), 0, 0),
+                        Position = vector.create(x, univ_vector.Y, z)
                     },
-	                    CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(10), 0)
+	                    CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(math.rad(10),0,0)
                     }
                     game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("RequestPlaceStructure"):InvokeServer(unpack(args))
+                else
+                    continue
                 end
 
 
-            else
-                continue
+
             end
+
         end
     end,
 })
