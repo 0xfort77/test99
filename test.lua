6438a3ff5ed32f6a8683a28f2cb35fbec0961ec3
0xfort77 <145624342+0xfort77@users.noreply.github.com>
2026-03-22 21:54:26 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@ if game.PlaceId == place_id or game.PlaceId == party_placeid then
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local _Version = "Cyan-99 v1.tbare90"
+local _Version = "Cyan-99 v1.tL+"
 
 local Window = Rayfield:CreateWindow({
    Name = _Version,
@@ -3122,7 +3122,7 @@ local logwallsButton = BaseTab:CreateButton({
                 CFrame = CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(90), 0),
                 Position = vector.create(x, univ_vector.Y, z)
             },
-                CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(90), 0)
+                CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(90) + 45, 0)
             }
             game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("RequestPlaceStructure"):InvokeServer(unpack(args))
 
