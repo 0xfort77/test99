b18ba4db461237389d8d3ac053623ff6766ed292
0xfort77 <145624342+0xfort77@users.noreply.github.com>
2026-03-22 22:19:47 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@ if game.PlaceId == place_id or game.PlaceId == party_placeid then
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local _Version = "Cyan-99 v1.tl50"
+local _Version = "Cyan-99 v1.tlookAt"
 
 local Window = Rayfield:CreateWindow({
    Name = _Version,
@@ -3119,10 +3119,10 @@ local logwallsButton = BaseTab:CreateButton({
                 game:GetService("Players").LocalPlayer.Inventory:FindFirstChild("Log Wall Blueprint"),
             {
                 Valid = true,
-                CFrame = CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(90), 0),
+                CFrame = CFrame.lookAt(CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(90), 0),CFrame.new(0,0,0)),
                 Position = vector.create(x, univ_vector.Y, z)
             },
-                CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(90) - 40, 0)
+                CFrame.lookAt(CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(90), 0),CFrame.new(0,0,0)) -- CFrame.new(xc, univ_cframe.Y, xz) * CFrame.Angles(0, math.rad(90) - 40, 0)
             }
             game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("RequestPlaceStructure"):InvokeServer(unpack(args))
 
