802c2df9e7ea6e81c0aa6c61c1d9af5cd9290ab5
0xfort77 <145624342+0xfort77@users.noreply.github.com>
2026-03-23 16:05:50 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@ if game.PlaceId == place_id or game.PlaceId == party_placeid then
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local _Version = "Cyan-99 v1.tSHprint"
+local _Version = "Cyan-99 v1.tSHtimerChild2"
 
 local Window = Rayfield:CreateWindow({
    Name = _Version,
@@ -186,7 +186,7 @@ local SHsection1 = LibTab:CreateSection("Misc:")
 local SHtimerLabel = LibTab:CreateLabel("Stronghold: not found", "timer")
 task.spawn(function()
     while true do
-        if stronghold_loc:FindFirstChild("Stronghold") then
+        if workspace.Map.Landmarks.Stronghold.Functional:FindFirstChild("Sign"):FindFirstChild("SurfaceGui") then
 
             local sh_display_time = workspace.Map.Landmarks.Stronghold.Functional.Sign.SurfaceGui.Frame.Body.Text
             print(sh_display_time)
