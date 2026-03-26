035d375f85cc21f88f3d6f572e35d3ace6c07b5d
0xfort77 <145624342+0xfort77@users.noreply.github.com>
2026-03-26 08:47:28 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,7 +5,7 @@ if game.PlaceId == place_id or game.PlaceId == party_placeid then
 
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local _Version = "Cyan-99 v1.tHoldB"
+local _Version = "Cyan-99 v1.tBedit"
 
 local Window = Rayfield:CreateWindow({
    Name = _Version,
@@ -3847,17 +3847,19 @@ local secretTextInput = ExtrasTab:CreateInput({
                                 local playerdead = true
                             end
 
-	                        --local args = {
-	                        --	beartrap
-	                        --}
-	                        --game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("RequestStartDraggingItem"):FireServer(unpack(args))
+	                        local args = {
+	                        	beartrap
+	                        }
+	                        game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("RequestStartDraggingItem"):FireServer(unpack(args))
 	                        beartrap:PivotTo(workspace:FindFirstChild(punish_player_index):FindFirstChild("HumanoidRootPart").CFrame * CFrame.new(0,-1,0))
-	                        --local args = {
-	                        --	beartrap
-	                        --}
-	                        --game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("StopDraggingItem"):FireServer(unpack(args))
+	                        local args = {
+	                        	beartrap
+	                        }
+	                        game:GetService("ReplicatedStorage"):WaitForChild("RemoteEvents"):WaitForChild("StopDraggingItem"):FireServer(unpack(args))
                             bear_prox.HoldDuration = 0
-                            task.wait()
+                            bear_prox.RequiresLineOfSight = false
+                            bear_prox.MaxActivationDistance = 100
+                            task.wait(0.099)
 	                        fireproximityprompt(bear_prox)
 	                        task.wait(0.44)
                         until playerdead
